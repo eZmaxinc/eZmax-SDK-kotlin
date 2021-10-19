b1bfef5c09498f87a42b27d0527e7e4a9eae24f9
Herrick <lordi@ezmax.ca>
2021-10-19 14:56:00 -0600

    Look for details in API documentation's changelog

--- src/main/kotlin/eZmaxApi/apis/ObjectEzsignfolderApi.kt
+++ src/main/kotlin/eZmaxApi/apis/ObjectEzsignfolderApi.kt
@@ -264,7 +264,7 @@ class ObjectEzsignfolderApi(basePath: kotlin.String = defaultBasePath) : ApiClie
 
     /**
     * Retrieve Ezsignfolder list
-    * ## ⚠️EARLY ADOPTERS WARNING  ### This endpoint is not officially released. Its definition might still change and it might not be available in every environment and region.  Enum values that can be filtered in query parameter *sFilter*:  | Variable | Valid values | |---|---| | eEzsignfolderStep | Unsent&lt;br&gt;Sent&lt;br&gt;PartiallySigned&lt;br&gt;Expired&lt;br&gt;Completed&lt;br&gt;Archived |
+    * Enum values that can be filtered in query parameter *sFilter*:  | Variable | Valid values | |---|---| | eEzsignfolderStep | Unsent&lt;br&gt;Sent&lt;br&gt;PartiallySigned&lt;br&gt;Expired&lt;br&gt;Completed&lt;br&gt;Archived |
     * @param eOrderBy Specify how you want the results to be sorted (optional)
     * @param iRowMax  (optional)
     * @param iRowOffset  (optional)
