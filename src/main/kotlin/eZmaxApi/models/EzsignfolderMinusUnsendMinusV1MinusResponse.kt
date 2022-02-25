d0b73354da6bdf04f9a43b903d3539cee118c231
Ian Lord <Ian.Lord@ezmax.ca>
2022-02-24 20:52:33 -0700

    Look for details in API documentation's changelog

--- src/main/kotlin/eZmaxApi/models/EzsignfolderMinusUnsendMinusV1MinusResponse.kt
+++ src/main/kotlin/eZmaxApi/models/EzsignfolderMinusUnsendMinusV1MinusResponse.kt
@@ -3,7 +3,7 @@
  *
  * This API expose all the functionnalities for the eZmax and eZsign applications.
  *
- * The version of the OpenAPI document: 1.1.4
+ * The version of the OpenAPI document: 1.1.5
  * Contact: support-api@ezmax.ca
  *
  * Please note:
